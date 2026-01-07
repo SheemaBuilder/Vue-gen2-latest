1562cf1ad628810b90b191a794892f8c6e1ac937
SheemaBuilder <sheema@builder.io>
2026-01-07 11:48:50 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello-world">
+    <div class="text">Hello {{ context.builderContent.data.title }}!</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped>
+.hello-world {
+  display: flex;
+  flex-direction: column;
+}
+.text {
+  padding: 50px;
+}
+</style>
---
+++ src/components/PageHeader.ts.vue
@@ -0,0 +1,79 @@
+<!-- <script setup lang="ts">
+//import { VContainer } from 'vuetify/components';
+import { computed } from 'vue';
+
+//import type { IBreadcrumbs } from '../components/cms/HBreadcrumb/HBreadcrumb.d';
+
+//import Breadcrumbs from '~vue/components/util/Breadcrumbs/Breadcrumbs.ts.vue';
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+interface RegistrationBreadcrumbs {
+  title?: string;
+  slug?: string;
+}
+
+interface Props {
+  title?: string;
+  subtitle?: string;
+  showBreadcrumbs?: boolean;
+  breadcrumbs?: RegistrationBreadcrumbs[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: undefined,
+  subtitle: undefined,
+  showBreadcrumbs: false,
+  breadcrumbs: () => ([]),
+});
+
+const breadcrumbItems = computed((): IBreadcrumbs[] => {
+  if (!props.breadcrumbs) return [];
+
+  const items: IBreadcrumbs[] = [{
+    text: 'Home',
+    to: '/',
+  }];
+
+  props.breadcrumbs.forEach(({ title, slug }: RegistrationBreadcrumbs, index: number) => {
+    if (!slug?.trim()) return;
+
+    items.push({
+      text: title?.trim() ?? '',
+      to: slug?.trim() ?? '',
+      disabled: index === props.breadcrumbs.length - 1,
+    });
+  });
+
+  return items;
+});
+</script>
+
+<template>
+  <VContainer id="page-header">
+    <Breadcrumbs
+      v-if="showBreadcrumbs && breadcrumbs.length"
+      class="dds-mb-4"
+      :items="breadcrumbItems"
+      data-za="breadcrumbs"
+    />
+    <hgroup>
+      <h1 class="text-h1">
+        {{ title }}
+      </h1>
+      <h2
+        v-if="subtitle"
+        class="dds-sub dds-mt-4 dds-mb-0 subtitle"
+      >
+        {{ subtitle }}
+      </h2>
+    </hgroup>
+  </VContainer>
+</template>
+<style scoped lang="scss">
+.subtitle {
+  font-size: $size-3;
+}
+</style> -->
